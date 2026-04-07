1cf86b98ae92d08a03c70d455a9b904e887ccdf7
thenewlegend <jiyonpj2501@gmail.com>
2026-04-07 19:56:26 +0530

    v2.65

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
   import Navbar from "$lib/components/layout/Navbar.svelte";
   import { dev } from "$app/environment";
   import { injectAnalytics } from "@vercel/analytics/sveltekit";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+  injectSpeedInsights();
 
   injectAnalytics({ mode: dev ? "development" : "production" });
   let { children } = $props();
