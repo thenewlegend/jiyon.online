320f6e181ff4953a0999757d33102840a1e2aea5
thenewlegend <jiyonpj2501@gmail.com>
2026-04-07 19:19:35 +0530

    v2.39

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-  import '../app.css';
-  import Navbar from '$lib/components/layout/Navbar.svelte';
+  import "../app.css";
+  import Navbar from "$lib/components/layout/Navbar.svelte";
+  import { dev } from "$app/environment";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+  injectAnalytics({ mode: dev ? "development" : "production" });
   let { children } = $props();
 </script>
 
